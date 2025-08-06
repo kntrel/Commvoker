f93f8fd468eecbaa367c1343119ce6d0121f306b
kntrel <me@kntrel.com>
2025-08-06 15:07:16 -0600

    Simplified AssemblerArgumentBinding

--- commvoker-core/src/main/java/com/kntrel/mc/commvoker/argument/binder/AssemblerArgumentBinding.java
+++ commvoker-core/src/main/java/com/kntrel/mc/commvoker/argument/binder/AssemblerArgumentBinding.java
@@ -8,6 +8,9 @@ import com.kntrel.mc.commvoker.argument.ArgumentBinding;
 import com.kntrel.mc.commvoker.argument.descriptor.ArgumentDescriptor;
 import com.kntrel.mc.commvoker.argument.descriptor.ArgumentNode;
 import com.kntrel.util.Priority;
+import com.kntrel.util.tuple.Pair;
+import com.kntrel.util.tuple.impl.SimplePair;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import java.lang.annotation.Annotation;
@@ -25,64 +28,39 @@ record AssemblerArgumentBinding<S, T>(
         Predicate<S> requirement
 ) implements ArgumentBinding<S, T> {
 
-    @Override
+    @Override @SuppressWarnings("unchecked")
     public ArgumentDescriptor<S, T> descriptor(ArgumentGatherer<? extends S> ctx) {
         Assembler<S, T> assembler = this.supplier.apply(ctx);
-        return new SimpleArgumentDescriptor<>(argumentNodes(assembler), new AssemblerContextualizer<>(assembler), this.requirement);
+        BiFunction<CommandContext<? extends S>, Object[], T> contextualizer = switch (assembler) {
+            case EndAssembler<?> end -> (c, o) -> (T) o[0];
+            case ComposedAssembler<? super S, T> comp -> comp::contextualize;
+        };
+        return new SimpleArgumentDescriptor<>(argumentNodes(assembler), contextualizer, this.requirement);
     }
 
-    @SuppressWarnings("unchecked")
-    private static <S, T> Collection<ArgumentNode<? super S, ?>> argumentNodes(Assembler<S, T> assembler) {
-        List<ArgumentNode<? super S, ?>> out = new LinkedList<>();
-        switch (assembler) {
-            case EndAssembler<?> e -> { out.add(new ArgumentNode<>(e.argumentType(), null)); }
 
-            case ComposedAssembler<S, T> c -> { for (var d : c.delegates()) {
-                LinkedList<ArgumentNode<? super S, ?>> delegateNodes = (LinkedList) argumentNodes(d.first());
-                SuggestionProvider<? super S> suggestionProvider = d.second();
-                if (!delegateNodes.isEmpty() && suggestionProvider != null) {
-                    ArgumentNode<? super S, ?> originalNode = delegateNodes.removeFirst();
-                    delegateNodes.addFirst(new ArgumentNode<>(originalNode.argumentType(), suggestionProvider));
+    private static <S> List<ArgumentNode<? super S, ?>> argumentNodes(Assembler<? super S, ?> root) {
+        Deque<Pair<Assembler<? super S, ?>, SuggestionProvider<? super S>>> stack = new ArrayDeque<>();
+        stack.addLast(new SimplePair<>(root, null));
+
+        List<ArgumentNode<? super S, ?>> out = new ArrayList<>();
+
+        while (!stack.isEmpty()) {
+            var pair = stack.pollLast();
+            var asm  = pair.first();
+            var sugg = pair.second();
+
+            switch (asm) {
+                case EndAssembler<?> end -> out.add(new ArgumentNode<>((ArgumentType<?>) end.argumentType(), sugg));
+                case ComposedAssembler<? super S, ?> comp -> {
+                    var delegates = comp.delegates();
+                    for (int i = delegates.size() - 1; i >= 0; i--) {
+                        var d = delegates.get(i);
+                        stack.addLast(new SimplePair<>(d.first(), d.second()));
+                    }
                 }
-                out.addAll(delegateNodes);
-            }}
-
-            default -> {}
+            }
         }
         return out;
-    }
-
-    private static class AssemblerContextualizer<S, T> implements BiFunction<CommandContext<? extends S>, Object[], T> {
-
-        private final Assembler<? super S, ? extends T> assembler_;
-
-        AssemblerContextualizer(Assembler<? super S, ? extends T> assembler) {
-            this.assembler_ = assembler;
-        }
-
-        @Override
-        public T apply(CommandContext<? extends S> ctx, Object[] objects) {
-            return this.consume(ctx, new LinkedList<>(Arrays.asList(objects)), this.assembler_);
-        }
-
-        @SuppressWarnings("unchecked")
-        private <I> I consume(CommandContext<? extends S> ctx, Queue<Object> objects, Assembler<? super S, I> assembler) {
-
-            if (assembler instanceof EndAssembler<?>) {
-                return (I) objects.poll();
-            }
-
-            ComposedAssembler<? super S, I> composed = (ComposedAssembler<? super S, I>) assembler;
-            var delegates = composed.delegates();
-            int size = delegates.size();
-            var iterator = delegates.iterator();
-
-            Object[] objs = new Object[size];
-            for (int i = 0; i < size; i++) {
-                objs[i] = this.consume(ctx, objects, iterator.next().first());
-            }
-
-            return composed.compose(ctx, objs);
-        }
     }
 }
