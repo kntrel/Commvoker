6386e252e14d096baf15ee4636baaa75f47b906f
kntrel <me@kntrel.com>
2025-08-06 19:41:40 -0600

    Rewrote ArgumentBindings using new Assembler model introduced in Core

--- commvoker-bukkit/src/main/java/com/kntrel/mc/commvoker/bukkit/BukkitCommvoker.java
+++ commvoker-bukkit/src/main/java/com/kntrel/mc/commvoker/bukkit/BukkitCommvoker.java
@@ -3,7 +3,6 @@ package com.kntrel.mc.commvoker.bukkit;
 import com.kntrel.mc.commvoker.argument.ArgumentRegistry;
 import com.kntrel.mc.commvoker.base.BaseCommvoker;
 import com.kntrel.mc.commvoker.bukkit.provided.BukkitArgumentBindings;
-import com.kntrel.mc.commvoker.bukkit.provided.BukkitImplicitBindings;
 import com.mojang.brigadier.CommandDispatcher;
 import org.bukkit.command.CommandSender;
 
@@ -12,7 +11,6 @@ public class BukkitCommvoker extends BaseCommvoker<CommandSender> {
         super(commandDispatcher);
 
         ArgumentRegistry<CommandSender> registry = this.getArgumentRegistry();
-        BukkitImplicitBindings.all().forEach(registry::register);
-        BukkitArgumentBindings.all().forEach(registry::register);
+        registry.register(BukkitArgumentBindings.all());
     }
 }
