188a024ba3bf23320b32ba757fa199c13a5b45f6
kntrel <me@kntrel.com>
2025-08-08 00:45:21 -0600

    Adding range for numbers and the ability to opt-out of auto-greedy on last String argument

--- commvoker-core/src/main/java/com/kntrel/mc/commvoker/provided/annotations/Size.java
+++ commvoker-core/src/main/java/com/kntrel/mc/commvoker/provided/annotations/Size.java
@@ -1,4 +1,4 @@
-package com.kntrel.mc.commvoker.annotation;
+package com.kntrel.mc.commvoker.provided.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,4 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface Word {}
+public @interface Size {
+    int value();
+}
