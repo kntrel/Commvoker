b98d9ee4eb89e96cd47fb7ae3a3435819c98862e
kntrel <me@kntrel.com>
2025-08-16 01:56:13 -0600

    Adapted SimpleArgumentDescriptor to the new ArgumentDescriptor interface

--- commvoker-core/src/main/java/com/kntrel/mc/commvoker/argument/binder/SimpleArgumentDescriptor.java
+++ commvoker-core/src/main/java/com/kntrel/mc/commvoker/argument/binder/SimpleArgumentDescriptor.java
@@ -1,6 +1,8 @@
 package com.kntrel.mc.commvoker.argument.binder;
 
 import com.kntrel.mc.commvoker.argument.binding.ArgumentDescriptor;
+import com.kntrel.mc.commvoker.argument.binding.CommandTemplate;
+import com.kntrel.mc.commvoker.argument.binding.Contextualizer;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import java.util.Collection;
@@ -8,7 +10,7 @@ import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 record SimpleArgumentDescriptor<S, T>(
-        Collection<ArgumentBuilder<? super S, ?>> argumentNodes,
-        BiFunction<CommandContext<? extends S>, Object[], T> contextualizer,
+        CommandTemplate.Node<S> argumentTrees,
+        Contextualizer<S, T> contextualizer,
         Predicate<S> requirement
 ) implements ArgumentDescriptor<S, T> {}
