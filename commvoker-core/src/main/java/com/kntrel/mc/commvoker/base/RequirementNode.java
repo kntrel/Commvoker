fb4736a84ddaca2547397ee8b0d3f2a6616345d2
kntrel <me@kntrel.com>
2025-09-14 00:19:00 -0600

    Added mandatory requirements

--- commvoker-core/src/main/java/com/kntrel/mc/commvoker/base/RequirementNode.java
+++ commvoker-core/src/main/java/com/kntrel/mc/commvoker/base/RequirementNode.java
@@ -14,48 +14,73 @@ class RequirementNode<S> implements Predicate<S> {
 
 
     //FIELDS
-    private final List<Predicate<S>> tests_;
-    private boolean passThrough_;
+    private final List<Predicate<S>> orTests_;
+    private final List<Predicate<S>> andTests_;
+    private boolean orAlways_;
 
 
     //CONSTRUCTORS
     public RequirementNode() {
-        this.tests_ = new ArrayList<>();
-        this.passThrough_ = false;
+        this.orTests_ = new ArrayList<>();
+        this.andTests_ = new ArrayList<>();
+        this.orAlways_ = false;
     }
 
 
     //IMPLEMENTATION
     @Override
     public boolean test(S s) {
-        if (this.passThrough_) { return true; }
-        if (this.tests_.isEmpty()) { return true; }
-        for (Predicate<S> test : this.tests_) {
-            if (test == null) { continue; }
-            if (test.test(s)) { return true; }
-        }
-        return false;
+        return this.testOrs(s) && this.testAnds(s);
     }
 
     @Override @SuppressWarnings("unchecked")
     public RequirementNode<S> or(Predicate<? super S> other) {
-        if (this.passThrough_) { return this; }
+        if (this.orAlways_) { return this; }
 
         if (other instanceof RequirementNode<?> node) {
-            if (node.passThrough_) {
+            if (node.orAlways_) {
                 return this.orAlways();
             } else {
-                node.tests_.forEach(t -> this.tests_.add((Predicate<S>) t));
+                node.orTests_.forEach(t -> this.orTests_.add((Predicate<S>) t));
             }
         } else {
-            this.tests_.add((Predicate<S>) other);
+            this.orTests_.add((Predicate<S>) other);
+        }
+        return this;
+    }
+
+    @Override @SuppressWarnings("unchecked")
+    public RequirementNode<S> and(Predicate<? super S> other) {
+        if (other instanceof RequirementNode<?> node) {
+            node.andTests_.forEach(t -> this.andTests_.add((Predicate<S>) t));
+        } else {
+            this.andTests_.add((Predicate<S>) other);
         }
         return this;
     }
 
     public RequirementNode<S> orAlways() {
-        this.passThrough_ = true;
-        this.tests_.clear();
+        this.orAlways_ = true;
+        this.orTests_.clear();
         return this;
+    }
+
+
+    //HELPERS
+    private boolean testOrs(S s) {
+        if (this.orAlways_) { return true; }
+        if (this.orTests_.isEmpty()) { return true; }
+        for (Predicate<S> test : this.orTests_) {
+            if (test == null) { continue; }
+            if (test.test(s)) { return true; }
+        }
+        return false;
+    }
+    private boolean testAnds(S s) {
+        for (Predicate<S> test : this.andTests_) {
+            if (test == null) { continue; }
+            if (!test.test(s)) { return false; }
+        }
+        return true;
     }
 }
--- commvoker-core/src/test/java/com/kntrel/mc/commvoker/base/RequirementNodeTest.java
+++ commvoker-core/src/test/java/com/kntrel/mc/commvoker/base/RequirementNodeTest.java
@@ -51,16 +51,59 @@ class RequirementNodeTest {
         node2.or(s -> s.endsWith("Z"));
 
         node1.or(node2);
-        assertTrue(node1.test("AZ")); // Matches both predicates
-        assertTrue(node1.test("A")); // Matches first predicate
-        assertTrue(node1.test("Z")); // Matches second predicate
-        assertFalse(node1.test("B")); // Matches neither
+        assertTrue(node1.test("AZ"));
+        assertTrue(node1.test("A"));
+        assertTrue(node1.test("Z"));
+        assertFalse(node1.test("B"));
     }
 
     @Test
     void testPassThroughBehavior() {
         RequirementNode<String> node = new RequirementNode<>();
         node.or(s -> s.equals("test")).orAlways();
-        assertTrue(node.test("anything")); // Pass-through is enabled
+        assertTrue(node.test("anything"));
+    }
+
+    @Test
+    void testSingleAndPredicate() {
+        RequirementNode<Integer> node = new RequirementNode<>();
+        node.and(i -> i > 10);
+        assertTrue(node.test(15));
+        assertFalse(node.test(5));
+    }
+
+    @Test
+    void testMultipleAndPredicates() {
+        RequirementNode<Integer> node = new RequirementNode<>();
+        node.and(i -> i > 10).and(i -> i % 2 == 0);
+        assertTrue(node.test(12));
+        assertFalse(node.test(15));
+        assertFalse(node.test(8));
+    }
+
+    @Test
+    void testAndWithAnotherRequirementNode() {
+        RequirementNode<Integer> node1 = new RequirementNode<>();
+        node1.and(i -> i > 10);
+
+        RequirementNode<Integer> node2 = new RequirementNode<>();
+        node2.and(i -> i % 2 == 0);
+
+        node1.and(node2);
+        assertTrue(node1.test(12));
+        assertFalse(node1.test(15));
+        assertFalse(node1.test(8));
+    }
+
+    @Test
+    void testCombinationOfOrsAndAnds() {
+        RequirementNode<Integer> node = new RequirementNode<>();
+        node.or(i -> i > 10).or(i -> i % 2 == 0);
+        node.and(i -> i < 20).and(i -> i != 15);
+
+        assertTrue(node.test(12));
+        assertFalse(node.test(22));
+        assertFalse(node.test(15));
+        assertFalse(node.test(9));
     }
 }
