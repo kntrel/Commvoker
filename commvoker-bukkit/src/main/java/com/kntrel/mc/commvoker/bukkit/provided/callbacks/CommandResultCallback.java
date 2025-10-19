19250b709f80173d83930606ee58d375d6e7e53f
kntrel <me@kntrel.com>
2025-10-19 13:48:40 -0600

    Added thrwos CommandSyntaxException to CommandResultCallback

--- commvoker-bukkit/src/main/java/com/kntrel/mc/commvoker/bukkit/provided/callbacks/CommandResultCallback.java
+++ commvoker-bukkit/src/main/java/com/kntrel/mc/commvoker/bukkit/provided/callbacks/CommandResultCallback.java
@@ -3,12 +3,13 @@ package com.kntrel.mc.commvoker.bukkit.provided.callbacks;
 import com.kntrel.mc.commvoker.bukkit.CommandResult;
 import com.kntrel.mc.commvoker.callback.ReturnCallback;
 import com.kntrel.mc.commvoker.command.CommandMethodContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import org.bukkit.command.CommandSender;
 
 public class CommandResultCallback implements ReturnCallback<CommandSender, CommandResult> {
     @Override
-    public void onReturn(CommandMethodContext<? extends CommandSender> context, CommandResult result) {
+    public void onReturn(CommandMethodContext<? extends CommandSender> context, CommandResult result) throws CommandSyntaxException {
         if (result.wasSuccessful()) {
             context.source().sendMessage(result.getMessage().getString());
         } else {
